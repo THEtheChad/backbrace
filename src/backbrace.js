687e9ec89098b9c9a877a59a016a88d8f0337e93
THEtheChad <chad.d.elliott@gmail.com>
2012-09-24 20:15:59 -0500

    updated to include reset and added tests

--- src/backbrace.js
+++ src/backbrace.js
@@ -2,10 +2,29 @@ Backbone.Collection = Backbone.Collection.extend({
   subCollection: function(opts){
     var sub = new Backbone.SubCollection(null, opts);
 
-    function subAdd   (model) { sub.sieve(model) && sub.__add__(model) }
-    function subRemove(model) { sub.__remove__(model) }
-    function subChange(model) {
-      if(sub.sieve(model)){
+    opts || (opts = {});
+
+    // Tack on our custom sieve
+    opts.sieve && (sub._sieve = opts.sieve);
+
+    // Store a reference to the parent
+    // (used in custom add/remove methods on sub)
+    sub.parent = this;
+
+    // Inherit parent methods
+    sub.__add__    = this.add;
+    sub.__remove__ = this.remove;
+
+    // Initialize the collection
+    _.each(this.models, function(m){ sub.subAdd(m) });
+
+    // Add our bindings
+    this.on('add'   , function(m){ sub.subAdd(m)     });
+    this.on('reset' , function(c){ sub.reset()       });
+    this.on('remove', function(m){ sub.__remove__(m) });
+
+    this.on('change', function(model){
+      if(sub._sieve(model)){
         // Check if the model is already a member of the subcollection
         if(!(sub._byCid[model.cid] || (model.id && sub._byId[model.id])))
           sub.__add__(model);
@@ -13,45 +32,50 @@ Backbone.Collection = Backbone.Collection.extend({
       else{
         sub.__remove__(model);
       }
-    }
-
-    // Tack on our custom sieve
-    opts.sieve && (sub.sieve = opts.sieve);
-
-    // Store a reference to the parent
-    // (used in custom add/remove methods on sub)
-    sub.parent = this;
-
-    // Inherit parent add/remove methods
-    sub.__add__    = this.add;
-    sub.__remove__ = this.remove;
-
-    // Initialize the collection
-    _.each(this.models, subAdd);
-
-    this.on('add'   , subAdd   );
-    this.on('remove', subRemove);
-    this.on('change', subChange);
+    });
 
     return sub;
   }
 });
 
 Backbone.SubCollection = Backbone.Collection.extend({
-  sieve: function(){return true},
-  // Adds the model to the main collection
-  // which triggers an event that will add
-  // it to the subcollection (if it passes the sieve)
-  add: function(models, options){
-    this.parent.add(models, options);
+  // Default sieve
+  _sieve: function(){
+    return true
   },
-  // Removes the model from the main collection
-  // which will fire an event to remove it
-  // from the subcollection
-  remove: function(models, options){
-    this.parent.remove(models, options);
+
+  sieve: function(func){
+    this._sieve = func;
+    this.reset();
+    return this;
   },
-  updateSieve: function(func){
-    this.sieve = func;
+
+  add: function(models, opts){
+    this.parent.add(models, opts);
+    return this;
+  },
+
+  remove: function(models, opts){
+    this.parent.remove(models, opts);
+    return this;
+  },
+
+  reset: function(opts){
+    var sub = this;
+
+    opts || (opts = {});
+
+    _.each(this.models, this._removeReference);
+    this._reset();
+    _.each(this.parent.models, function(model){
+      sub.subAdd(model, opts);
+    });
+    if(!opts.silent) this.trigger('reset', this, opts);
+    return this;
+  },
+
+  subAdd: function(model, opts){
+    this._sieve(model) && this.__add__(model, opts);
+    return this;
   }
 });
